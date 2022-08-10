b4045d1dbc4ec041f6f5a519a67f5f0dc2626680
Reza <alialreza98@gmail.com>
2022-08-11 05:48:39 +0600

    final product

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -23,6 +23,34 @@ const ProductFeed = ({ products }) => {
         src="https://links.papareact.com/dyz"
         alt=""
       />
+      <div className=" md:col-span-2 ">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
     </div>
   );
 };
