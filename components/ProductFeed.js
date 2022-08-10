aaecce354fd6aca24322a0f85b27b1fb6f0c1dc3
Reza <alialreza98@gmail.com>
2022-08-11 05:42:19 +0600

    4 products and add then more product with slice

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -1,19 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
 import Product from "./Product";
 
 const ProductFeed = ({ products }) => {
   return (
-    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-      {products.map(({ id, title, price, description, category, image }) => (
-        <Product
-          key={id}
-          id={id}
-          title={title}
-          price={price}
-          description={description}
-          category={category}
-          image={image}
-        />
-      ))}
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto ">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+
+      <img
+        className="  md:col-span-4 "
+        src="https://links.papareact.com/dyz"
+        alt=""
+      />
     </div>
   );
 };
