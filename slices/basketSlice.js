c828f243048316c391a75e83de453ca0e2cce8a1
Reza <alialreza98@gmail.com>
2022-08-17 04:33:11 +0600

    initial stage for redux

---
+++ slices/basketSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState={
+    items:[],
+};
+
+
+export const basketSlice= createSlice({
+    name:'basket',
+    initialState,
+    reducers:{
+        // two action function
+        addToBasket:{state,action}=>{},
+        removeFromBasket: {state,action}=>{},
+    },
+})
+
+// to use this actions or function another component
+export const {addToBasket,removeFromBasket} = basketSlice.actions;
+
+
+// selector help us to fetch the data from the basket
+export const selectItems = {state} =>state.basket.items;
+
+export default basketSlice.reducer;
