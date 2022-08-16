ab23409ab3f9c8eac1c5690c6ce3d6aae3539ef0
Reza <alialreza98@gmail.com>
2022-08-16 21:08:59 +0600

    problem solve with safeHydrate

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -1,31 +1,47 @@
 /* eslint-disable @next/next/no-img-element */
 import Product from "./Product";
+import SafeHydrate from "./SafeHydrate";
 
 const ProductFeed = ({ products }) => {
   return (
-    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto ">
-      {products
-        .slice(0, 4)
-        .map(({ id, title, price, description, category, image }) => (
-          <Product
-            key={id}
-            id={id}
-            title={title}
-            price={price}
-            description={description}
-            category={category}
-            image={image}
-          />
-        ))}
-
-      <img
-        className="  md:col-span-4 "
-        src="https://links.papareact.com/dyz"
-        alt=""
-      />
-      <div className=" md:col-span-2 ">
+    <SafeHydrate>
+      <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto ">
         {products
-          .slice(4, 5)
+          .slice(0, 4)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+
+        <img
+          className="  md:col-span-4 "
+          src="https://links.papareact.com/dyz"
+          alt=""
+        />
+        <div className=" md:col-span-2 ">
+          {products
+            .slice(4, 5)
+            .map(({ id, title, price, description, category, image }) => (
+              <Product
+                key={id}
+                id={id}
+                title={title}
+                price={price}
+                description={description}
+                category={category}
+                image={image}
+              />
+            ))}
+        </div>
+        {products
+          .slice(5, products.length)
           .map(({ id, title, price, description, category, image }) => (
             <Product
               key={id}
@@ -38,20 +54,7 @@ const ProductFeed = ({ products }) => {
             />
           ))}
       </div>
-      {products
-        .slice(5, products.length)
-        .map(({ id, title, price, description, category, image }) => (
-          <Product
-            key={id}
-            id={id}
-            title={title}
-            price={price}
-            description={description}
-            category={category}
-            image={image}
-          />
-        ))}
-    </div>
+    </SafeHydrate>
   );
 };
 
